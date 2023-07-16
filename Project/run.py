bef1db8a2865e36c14a04df7770b52f7e258b1d1
zakelyk <candra.setyo.w@gmail.com>
2023-07-16 19:57:51 +0700

    Update run.py

--- Project/run.py
+++ Project/run.py
@@ -1,9 +1,10 @@
-import create_line
-import create_ellipse
-import transformasi
+from create_line import create_line
+from create_ellipse import create_ellipse
+from transformasi import transform_object
+# from windowing import windowing
+# from clipping import clipping
 
 def main():
-    lines = []
     while True:
         print("=== Program Grafika Komputer ===")
         print("1. Membuat Garis")
@@ -20,18 +21,23 @@ def main():
             create_line()
         elif choice == 2:
             print("=== Membuat Ellipse ===")
-            create_ellipse
+            create_ellipse()
         elif choice == 3:
             print("=== Transformasi ===")
-            transformasi()
+            transform_object()
         elif choice == 4:
             print("=== Windowing (Objek berupa garis) ===")
-
+            # windowing
+            print("fitur belum tersedia")
         elif choice == 5:
             print("=== Clipping (Objek berupa garis) ===")
-
+            # clipping()
+            print("Fitur belum tersedia")
         elif choice == 6:
             print("Terima kasih telah menggunakan program.")
             break
         else:
             print("Pilihan tidak valid. Silakan masukkan pilihan yang benar.")
+
+if __name__ == '__main__':
+    main()
