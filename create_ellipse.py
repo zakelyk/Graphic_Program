9cca70886104cf9f4f7e5f3abf4ac0b646a51199
zakelyk <candra.setyo.w@gmail.com>
2023-07-17 04:23:29 +0700

    Update create_ellipse.py

--- create_ellipse.py
+++ create_ellipse.py
@@ -4,7 +4,7 @@ def create_ellipse_from_keyboard():
     xc = int(input("Masukkan koordinat x pusat ellipse: "))
     yc = int(input("Masukkan koordinat y pusat ellipse: "))
     r = int(input("Masukkan jari-jari ellipse: "))
-    return xc, yc, r
+    return [(xc, yc), (r,r)]
 
 def create_ellipse_from_mouse():
     fig, ax = plt.subplots()
@@ -15,15 +15,15 @@ def create_ellipse_from_mouse():
     xc = int(coords[0][0])
     yc = int(coords[0][1])
     r = int(coords[1][0] - coords[0][0])
-    return xc, yc, r
+    return [(xc, yc), (r,r)]
 
 def print_ellipse_coordinates(coordinates):
-    xc, yc, r = coordinates
+    [(xc, yc), (r,r)] = coordinates
     print("Koordinat pusat ellipse: ({}, {})".format(xc, yc))
     print("Jari-jari ellipse:", r)
 
 def plot_ellipse(coordinates):
-    xc, yc, r = coordinates
+    [(xc, yc), (r,r)] = coordinates
 
     x = 0
     y = r
