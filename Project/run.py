fe67076cc8241f0201aa3937df5bfc5cb091e972
zakelyk <candra.setyo.w@gmail.com>
2023-07-16 20:18:13 +0700

    Update run.py

--- Project/run.py
+++ Project/run.py
@@ -1,43 +1,32 @@
-from create_line import create_line
-from create_ellipse import create_ellipse
-from transformasi import transform_object
-# from windowing import windowing
-# from clipping import clipping
 
 def main():
-    while True:
-        print("=== Program Grafika Komputer ===")
+    choice = 0
+
+    while choice != 6:
+        print("=== Menu Pilihan Fitur ===")
         print("1. Membuat Garis")
         print("2. Membuat Ellipse")
-        print("3. Transformasi")
+        print("3. Transformasi Objek")
         print("4. Windowing")
         print("5. Clipping")
         print("6. Keluar")
 
-        choice = int(input("Masukkan pilihan (1-6): "))
+        choice = int(input("Pilih fitur (1-6): "))
 
         if choice == 1:
-            print("=== Membuat Garis ===")
-            create_line()
+            import create_line
         elif choice == 2:
-            print("=== Membuat Ellipse ===")
-            create_ellipse()
+            import create_ellipse
         elif choice == 3:
-            print("=== Transformasi ===")
-            transform_object()
+            import transformasi
         elif choice == 4:
-            print("=== Windowing (Objek berupa garis) ===")
-            # windowing
-            print("fitur belum tersedia")
+            print("Fitur Belum tersedia")
         elif choice == 5:
-            print("=== Clipping (Objek berupa garis) ===")
-            # clipping()
             print("Fitur belum tersedia")
         elif choice == 6:
-            print("Terima kasih telah menggunakan program.")
-            break
+            print("Terima kasih telah menggunakan program ini.")
         else:
-            print("Pilihan tidak valid. Silakan masukkan pilihan yang benar.")
+            print("Pilihan tidak tersedia.")
 
-if __name__ == '__main__':
-    main()
+# Jalankan program utama
+main()
